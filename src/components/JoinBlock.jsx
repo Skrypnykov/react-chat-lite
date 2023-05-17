0a4bee6f74a337e1cbebec4ffe224e2ff932c19e
Skrypnykov <oskripnikov@gmail.com>
2023-05-17 13:26:18 +0300

    second commit

--- src/components/JoinBlock.jsx
+++ src/components/JoinBlock.jsx
@@ -1,12 +1,45 @@
 import React from "react";
-import socket from "../socket";
+import axios from "axios";
 
-function JoinBlock() {
+function JoinBlock({ onLogin }) {
+  const [roomId, setRoomId] = React.useState("");
+  const [userName, setUserName] = React.useState("");
+  const [isLoading, setLoading] = React.useState(false);
+
+  const onEnter = async () => {
+    if (!roomId || !userName) {
+      return alert("Відсутні дані");
+    }
+    const obj = {
+      roomId,
+      userName,
+    };
+    setLoading(true);
+    await axios.post("/rooms", obj);
+    onLogin(obj);
+  };
+
   return (
     <div className="join-block">
-      <input type="text" placeholder="ID кімнати" />
-      <input type="text" placeholder="Ваше ім'я" />
-      <button className="btn btn-success">Увійти</button>
+      <input
+        type="text"
+        placeholder="ID кімнати"
+        value={roomId}
+        onChange={(e) => setRoomId(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Ваше ім'я"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <button
+        disabled={isLoading}
+        onClick={onEnter}
+        className="btn btn-success"
+      >
+        {isLoading ? "ВХІД..." : "УВІЙТИ"}
+      </button>
     </div>
   );
 }
